8b885c9c5a669f14eb026dc5991a025c3adae514
luc12001 <luc12001@byui.edu>
2022-01-17 13:58:45 -0600

    part 1 of this weeks studies, added models and controllers and data

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errorController = require('.controller/error.js');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,8 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) =>{
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
+
 
 app.listen(3000);
