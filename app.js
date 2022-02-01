74171fcf77b78c1b36e9224d2ea451b2e365a1dc
luc12001 <luc12001@byui.edu>
2022-02-01 11:17:48 -0600

    Went through chapter about sessions and cookies and followed examples and added code.

--- app.js
+++ app.js
@@ -1,53 +1,69 @@
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
 const cors = require('cors');
+
 const corsOptions = {
-    origin: "https://store341.herokuapp.com/",
-    optionsSuccessStatus: 200
+  origin: "https://<your_app_name>.herokuapp.com/",
+  optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
 
 const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    family: 4
+  family: 4
 };
 
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://Brandon:Admin@cluster0.qqma9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-                        
-
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://Brandon:Admin@cluster0.qqma9.mongodb.net/myFirstDatabase';
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+  url: MONGODB_URL,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  }));
 
 app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+  .then(user => {
       req.user = user;
       next();
-    })
-    .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
@@ -68,7 +84,7 @@ mongoose
         user.save();
       }
     });
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch(err => {
     console.log(err);
