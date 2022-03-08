7e99131f1174d4887d2bc0c3c179431734c0709b
luc12001 <luc12001@byui.edu>
2022-03-08 12:55:26 -0600

    Week 10

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
 const cors = require('cors');
+const multer = require('multer');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -19,6 +20,27 @@ const path = require('path');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 const csrfProtection = csrf();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if(file.mimetype === 'image/png' 
+  || file.mimetype === 'image/jpg' 
+  || file.mimetype === 'image/jpeg') {
+
+    cb(null, true);
+  } else {
+  
+  cb(null, false);
+  }
+};
 
 const app = express();
 const store = new MongoDBStore({
@@ -42,10 +64,10 @@ app.set('views', 'views');
 
 
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(app.join(__dirname, 'images'))));
 app.use(
   session({
     secret: 'my secret',
